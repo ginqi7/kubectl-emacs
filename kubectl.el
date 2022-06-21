7b6fd1d291cdf88e01ae1fee61b167f7739461e1
JinQiqi <jinqiqi@luren-ali-mac.local>
2022-06-21 20:01:59 +0800

    feat: add function to  or  file in pod

--- kubectl.el
+++ kubectl.el
@@ -13,8 +13,8 @@
       "p" #'k8s-get-pods-current-iterm
       "d" #'k8s-get-deployments-current-iterm
       "c" #'k8s-get-configMaps-current-iterm
-      "i" #'k8s-get-ingresses-current-iterm
-      )))
+      "i" #'k8s-get-ingresses-current-iterm)
+    "i" #'k8s-list-pod-files-current-iterm))
 
 (defun k8s--buffer-whole-string (buffer)
   "Get String without properties from other buffer"
@@ -26,16 +26,14 @@
 (defun k8s--output-to-local-buffer-variable (proc string)
   "Get Asyn process's output to k8s--asyn-process-output variable"
   (when (buffer-live-p (process-buffer proc))
-    (setq k8s--asyn-process-output (concat k8s--asyn-process-output string))))
+    (setq k8s--asyn-process-output (concat k8s--asyn-process-output (replace-regexp-in-string "\015" "" (ansi-color-apply string))))))
 
 (defun k8s--split-string-wtith-blank (str)
   "Split String with space, and remove all empty element"
-  (remove "" (split-string str "\s"))
-  )
+  (remove "" (split-string str "\s")))
 
 (defun k8s--create-buffer-keymap (buffer)
-  k8s-namespace-buffer-keymap
-  )
+  k8s-namespace-buffer-keymap)
 
 (defun k8s--list-all-async (process signal)
   "Run kubectl Async and list resource"
@@ -55,21 +53,71 @@
                 :filter #'k8s--output-to-local-buffer-variable
                 :sentinel 'k8s--list-all-async))
 
+(defun k8s--list-files-async (process signal)
+  "Run kubectl Async and list resource"
+  (when (memq (process-status process) '(exit))
+    (let* ((output (remove nil (mapcar 'k8s--split-string-wtith-blank (split-string k8s--asyn-process-output "\n"))))
+          (titles (list "auth" "type" "user" "user" "size" "time" "time" "time" "name"))
+          (data (cdr output))
+          (the-buffer (process-buffer process)))
+      (k8s--show-ctable titles data the-buffer (process-command process) (k8s--create-buffer-keymap the-buffer)))))
+
+(defun k8s--list-files (command)
+  (make-process :name (format "%s" command)
+                :buffer (format "%s" command)
+                :command command
+                :filter #'k8s--output-to-local-buffer-variable
+                :sentinel #'k8s--list-files-async))
+
+(defun k8s--run-shell-on-pod(ns pod pod-cmd)
+  (k8s--list-files (list "kubectl" "-n" ns "exec" "-it" pod "--" "bash" "-i" "-c" pod-cmd)))
+
 (defun k8s--create-cmodel (title)
   "Create cmodel by title string"
   (make-ctbl:cmodel :title  title
                     :align 'left))
 
+
+(defun k8s--open-pod-inner-file-cmd(file-name cmd)
+  (setq k8s--asyn-process-output nil)
+  (message (format "%s" cmd))
+  (make-process :name file-name
+                :buffer file-name
+                :command cmd
+                :filter #'k8s--output-to-local-buffer-variable
+                :sentinel #'k8s--switch-to-pod-inner-file-buffer))
+
+(defun k8s--open-pod-inner-file (ns pod path)
+  (let ((cmd (list "kubectl" "-n" ns "exec" "-it" pod "--" "bash" "-i" "-c" (concat "cat " path))))
+    (k8s--open-pod-inner-file-cmd path cmd)))
+
+
+(defun k8s--add-pod-file-click-hook (command data)
+  (let* ((ns-idx (+ 1 (cl-position "-n" command :test #'equal)))
+        (ns (nth ns-idx command))
+        (pod-idx (+ 1 (cl-position "-it" command :test #'equal)))
+        (pod (nth pod-idx command))
+        (pod-cmd (car (last command)))
+        (file-name (car (last data)))
+        (file-path (concat (car (last (split-string pod-cmd " +" t))) file-name))
+        (type (car data)))
+    (if (string-prefix-p "d" type)
+        (k8s--list-files (append (butlast command) (list (concat pod-cmd file-name "/"))))
+      (k8s--open-pod-inner-file ns pod file-path))))
+
+(defun k8s--add-diffent-click-hook (command data)
+  (if (string-match-p "ls " (car (last command)))
+      (k8s--add-pod-file-click-hook command data)
+    (k8s--get-yaml (car data) (append command (list (car data) "-o" "yaml")))))
+  
+
 (defun k8s--add-click-hook (componet command)
   "Add click-hook for ctable"
-  (lexical-let ((cp componet)
+  (lexical-let* ((cp componet)
                 (cmd command))
     (ctbl:cp-add-click-hook 
      componet 
-     (lambda () (let ((selected-val (car (ctbl:cp-get-selected-data-row cp))))
-              (setq k8s--asyn-process-output nil)
-              (k8s--get-yaml selected-val (append cmd (list selected-val "-o" "yaml")))
-               )))))
+     (lambda () (k8s--add-diffent-click-hook cmd (ctbl:cp-get-selected-data-row cp))))))
 
 
 (defun k8s--show-ctable (titles data buffer command keymap)
@@ -84,10 +132,9 @@
           :custom-map keymap
           :buffer buffer
           :model model)))
-
+    (setq k8s--asyn-process-output nil)
     (pop-to-buffer (ctbl:cp-get-buffer component))
     (k8s--add-click-hook component command)
-    (message "%s" (ctbl:table-mode-map keymap))
     (k8s--add-help-info (k8s--keymaps-to-data (ctbl:table-mode-map keymap)))))
 
 (defun k8s--butify-key (key)
@@ -103,8 +150,7 @@
   (if (not (eq key-map 'keymap)) 
       (if (listp (cdr key-map))
           (k8s--combine-key (k8s--butify-key (car key-map)) (remove nil (mapcar #'k8s--keymap-to-item (cdr key-map))))
-        (list (k8s--butify-key (car key-map)) (cdr key-map))
-        )))
+        (list (k8s--butify-key (car key-map)) (cdr key-map)))))
 
 (defun k8s--flat-list (the-list)
   (let ((mutil (car (seq-filter (lambda(item) (listp (car item))) the-list)))
@@ -131,9 +177,7 @@
         (setq str-list (cdr str-list))
         (if (eq (point) (point-max))
             (insert "\n")
-            (next-line)
-          )
-        ))))
+            (next-line))))))
 
 (defun k8s--add-help-info (data)
   "Insert Help Info"
@@ -154,6 +198,11 @@
     (switch-to-buffer (process-buffer process))
     (insert k8s--asyn-process-output)
     (yaml-mode)))
+
+(defun k8s--switch-to-pod-inner-file-buffer(process signal)
+  (when (memq (process-status process) '(exit))
+    (switch-to-buffer (process-buffer process))
+    (insert k8s--asyn-process-output)))
 
 (defun k8s--get-yaml (buffer-name shell-command)
   (setq k8s--asyn-process-output nil)
@@ -218,6 +267,9 @@
   (interactive)
   (k8s-get-resource ns "pods" pod t))
 
+(defun k8s-list-pod-files (ns pod path)
+  (k8s--run-shell-on-pod ns pod (concat "ls -lah " path)))
+
 (defun k8s-apply-current-yaml-buffer ()
   "Using kubectl apply current yaml buffer"
   (interactive)
@@ -243,6 +295,15 @@
 (defun k8s-get-configMaps-current-iterm ()
   (interactive)
   (k8s--run-function-in-current-iterm #'k8s-get-configMaps))
+
+(defun k8s-list-pod-files-current-iterm ()
+  (interactive)
+  (let* ((cp (ctbl:cp-get-component))
+         (buff-name (buffer-name (ctbl:cp-get-buffer cp)))
+         (ns (car (split-string buff-name ":")))
+         (pod (car (ctbl:cp-get-selected-data-row cp)))
+         )
+    (k8s-list-pod-files ns pod "/home/admin/")))
 
 (provide 'kubectl)
 ;;; kubectl.el ends here
